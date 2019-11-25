1124ae1e0d1270167c3607a8dd8a9cdf715e7006
Sourabh Das <sourabh.das@codeclouds.in>
2019-11-25 19:58:59 +0530

    edit 1

--- routes/admin.js
+++ routes/admin.js
@@ -16,14 +16,17 @@ router.get('/list/user', UserController.getList);
 router.get('/listing/user', UserController.getListing);
 router.get('/add/user/', UserController.create);
 router.post('/save/user/', UserController.save);
+router.get('/edit/user/:id', UserController.edit);
 
 router.get('/list/city', CityController.getList);
 router.get('/listing/city', CityController.getListing);
 router.get('/add/city/', CityController.create);
 router.post('/save/city/', CityController.save);
+router.get('/edit/city/:id', CityController.edit);
 
 router.get('/list/service', ServiceController.getList);
 router.get('/listing/service', ServiceController.getListing);
 router.get('/add/service/', ServiceController.create);
 router.post('/save/service/', ServiceController.save);
+router.get('/edit/service/:id', ServiceController.edit);
 module.exports = router
